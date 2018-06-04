111a0c8fc4ba3c500a83cb66ce2758087f877f48
Joan Sandoval Codina <joansando@gmail.com>
2018-06-04 13:36:47 +0200

    Updated to be compatible with new OF versions

--- src/ofxTLKeyframes.cpp
+++ src/ofxTLKeyframes.cpp
@@ -913,7 +913,9 @@ void ofxTLKeyframes::simplifySelectedKeyframes( float tolerance ){
 
     if ( selectedKeyframes.size() > 2 )
     {
-         vector<ofPoint> pts;
+//         vector<ofPoint> pts;
+        vector<glm::vec3> pts; // modified
+
         float startTime = (float)selectedKeyframes[0]->time;
         float timeNormalizationFactor = 1.0 / (float)selectedKeyframes.size();
         for(int k = 0; k < selectedKeyframes.size(); k++){
